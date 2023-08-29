e0969fee04e40a121e3df99bf34bd7f74908f932
Denis <denryzhak@gmail.com>
2023-08-29 23:31:22 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import App from "app/App";
 import {BrowserRouter} from "react-router-dom";
 import 'app/styles/index.scss'
 import ThemeProvider from "app/providers/ThemeProvider/ui/ThemeProvider";
-
+console.log('test')
 render(
     <BrowserRouter>
         <ThemeProvider>
