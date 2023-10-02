ed00c3b3e1c2eb9b894b3710408abd72f0b79524
ryzhaknode <denryzhak@gmail.com>
2023-10-02 21:42:56 +0300

    l

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Theme, useTheme } from '../../../../app/providers/ThemeProvider';
 import LightIcon from '../../../assets/icons/theme-light.svg';
 import DarkIcon from '../../../assets/icons/theme-dark.svg';
-import { Button, ThemeButton } from '../../Button/Button';
+import { Button, ButtonTheme } from '../../Button/Button';
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -14,7 +14,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
 
     return (
         <Button
-            theme={ThemeButton.CLEAR}
+            theme={ButtonTheme.CLEAR}
             className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
