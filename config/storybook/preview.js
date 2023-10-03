f6c16d7d032ab5f9333c9f65057847dbf37344d0
ryzhaknode <denryzhak@gmail.com>
2023-10-03 10:41:28 +0300

    l

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,8 +1,8 @@
 import { addDecorator } from '@storybook/react';
-import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from '../../src/app/providers/ThemeProvider';
-import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { StyleDecorator } from '../../../../Desktop/ulbi-reactfrontend/code/29 Модальное окно. Portal/29/production-project-275ffcccdf0d6e889dc640425bb37336eb8dca5f/src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../../../Desktop/ulbi-reactfrontend/code/29 Модальное окно. Portal/29/production-project-275ffcccdf0d6e889dc640425bb37336eb8dca5f/src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../../../Desktop/ulbi-reactfrontend/code/29 Модальное окно. Portal/29/production-project-275ffcccdf0d6e889dc640425bb37336eb8dca5f/src/app/providers/ThemeProvider';
+import { RouterDecorator } from '../../../../Desktop/ulbi-reactfrontend/code/29 Модальное окно. Portal/29/production-project-275ffcccdf0d6e889dc640425bb37336eb8dca5f/src/shared/config/storybook/RouterDecorator/RouterDecorator';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
